c773307677857d9e5244a5fae2f4fb8c3742cc3a
LDVSOFT <lapshin.dv@mail.ru>
2017-11-02 17:24:55 +0300

    WIP: Queues, LockFree fails

---
+++ hw6/hw6/IBlockingArrayQueue.cs
@@ -0,0 +1,13 @@
+﻿namespace Hw6
+{
+    public interface IBlockingArrayQueue<T>
+    {
+        void Enqueue(T element);
+        bool TryEnqueue(T element);
+
+        T Dequeue();
+        (bool succeeded, T value) TryDequeue();
+
+        void Clear();
+    }
+}
